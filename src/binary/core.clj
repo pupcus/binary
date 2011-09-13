d3fd591a44314da278d3e2c304269fd76ace1bf2
Michael Pendergrass <mdpendergrass@gmail.com>
2011-09-13 09:59:54 -0400

    many changes to use maps for form definitions; clean up naming and merge all in to core file;

--- src/binary/core.clj
+++ src/binary/core.clj
@@ -1,4 +1,10 @@
-(ns binary.core)
+(ns binary.core
+  (:refer-clojure :exclude [read])
+  (:import java.nio.ByteOrder))
+
+;;
+;; various sizes and utilities for sizes
+;;
 
 (def bits-in-octet 8)
 
@@ -9,6 +15,30 @@
 (def long-size    (/ java.lang.Long/SIZE bits-in-octet))
 (def float-size   (/ java.lang.Float/SIZE bits-in-octet))
 (def double-size  (/ java.lang.Double/SIZE bits-in-octet))
+
+(def sizes
+  {
+   :int     int-size
+   :long    long-size
+   :float   float-size
+   :double  double-size
+   :cstring byte-size
+   :vstring byte-size
+   })
+
+
+;;
+;; endian byte orders
+;;
+
+(def byte-order
+     {
+      :big    ByteOrder/BIG_ENDIAN
+      :little ByteOrder/LITTLE_ENDIAN
+      })
+
+;;
+;; utilities for building java byte arrays (byte[])
 
 (defn as-byte [i]
   (byte (let [i (bit-and 0xFF i)]
@@ -54,3 +84,143 @@
 (defn bit-set? [bn n]
   (let [check (bit-shift-left (long 1) bn)]
     (= (bit-and n check) check)))
+
+;;
+;; multimethod for extracting value of a param
+;; could be value, keyword reference, or function
+;; could be param map of :in and :out values
+;;
+
+
+(defn value-dispatch-fn [target data direction]
+  (class target))
+
+(defmulti value value-dispatch-fn)
+
+(defmethod value clojure.lang.PersistentArrayMap [{:keys [out in]} data direction]
+  (cond
+   (and out (= direction :writing)) (value out data :writing) 
+   (and in  (= direction :reading)) (value in data :writing)
+   :else data))
+
+(defmethod value :default [target data direction]
+  (if (or (fn? target)
+          (keyword? target))
+    (target data)
+    target))
+
+
+;;
+;; methods for reading binary data buffers
+;;
+
+(defn read-dispatch [_ {:keys [type]}] type)
+
+(defmulti read read-dispatch)
+
+(defmethod read :int [buf {:keys [endian] :or {endian :big}}]
+  (.order buf (endian byte-order))
+  (.getInt buf))
+
+(defmethod read :long [buf {:keys [endian] :or {endian :big}}]
+  (.order buf (endian byte-order))
+  (.getLong buf))
+
+(defmethod read :float [buf {:keys [endian] :or {endian :big}}]
+  (.order buf (endian byte-order))
+  (.getFloat buf))
+
+(defmethod read :double [buf {:keys [endian] :or {endian :big}}]
+  (.order buf (endian byte-order))
+  (.getDouble buf))
+
+(defmethod read :cstring [buf {:keys [size]}]
+  (if (nil? size) (throw (Exception. "size required for cstring type")))
+  (loop [i (dec size)
+         s (conj [] (.get buf))]
+    (if (or (= (last s) 0)
+            (<= i 0))
+      (String. (to-byte-array (if (= (last s) 0) (butlast s) s)))
+      (recur (dec i) (conj s (.get buf))))))
+
+(defmethod read :vstring [buf {:keys [size]}]
+  (if (nil? size) (throw (Exception. "size required for cstring type")))
+  (let [size (min (.getInt buf) size)
+        result (byte-array size)
+        _      (.get buf result 0 size)]
+    (String. result)))
+
+(defn decode-buffer [buf {:keys [number] :or {number 1} :as form}]
+  (let [vec (into [] (for [_ (range number)] (read buf form)))]
+    (if (= number 1)
+      (first vec)
+      vec)))
+
+(defn process-form-decode [buf {:keys [name number size in] :as form} sofar]
+  (let [form (merge form (if number {:number (value number sofar :reading)}) (if size {:size (value size sofar :reading)}))
+        data (decode-buffer buf form)]
+    (if name
+      [name (if in (in (merge sofar {name data})) data)]
+      nil)))
+
+(defn decode [buf forms]
+  (loop [sofar {}
+         forms forms]
+    (if (nil? (seq forms))
+      sofar
+      (recur (conj sofar (process-form-decode buf (first forms) sofar)) (rest forms)))))
+
+;;
+;; methods for writing binary data buffers
+;;
+
+(defn write-dispatch [_ {:keys [type]} _] type)
+
+(defmulti write write-dispatch)
+
+(defmethod write :int [buf {:keys [endian] :or {endian :big}} data]
+  (.order buf (endian byte-order))
+  (.putInt buf data))
+
+(defmethod write :long [buf {:keys [endian] :or {endian :big}} data]
+  (.order buf (endian byte-order))
+  (.putLong buf data))
+
+(defmethod write :float [buf {:keys [endian] :or {endian :big}} data]
+  (.order buf (endian byte-order))
+  (.putFloat buf data))
+
+(defmethod write :double [buf {:keys [endian] :or {endian :big}} data]
+  (.order buf (endian byte-order))
+  (.putDouble buf data))
+
+(defmethod write :cstring [buf {:keys [size]} data]
+  (let [bytes (.getBytes data)]
+    (.put buf bytes 0 (min (count bytes) (dec size)))
+    (.put buf (byte 0))))
+
+(defmethod write :vstring [buf {:keys [size]} data]
+  (let [bytes (.getBytes data)
+        size (min (count bytes) size)]
+    (.putInt buf size)
+    (.put buf bytes 0 size)))
+
+(defn encode-buffer [buf {:keys [number] :or {number 1} :as form} data]
+  (if (> number 1)
+    (doseq [i (range number)] (let [piece (nth data i)] (write buf form piece)))
+    (write buf form data)))
+
+(defn process-form-encode [buf {:keys [name number size out] :as form} datamap]
+  (let [form (merge form (if number {:number (value number datamap :writing)}) (if size {:size (value size datamap :writing)}))
+        data (name datamap)
+        data (if out (out (merge datamap {name data})) data)]
+    (encode-buffer buf form data)
+    {name data}))
+
+(defn encode [buf forms datamap]
+  (loop [sofar datamap
+         forms forms]
+    (if (nil? (seq forms))
+      buf
+      (recur (merge sofar (process-form-encode buf (first forms) sofar)) (rest forms)))))
+
--- test/binary/test/core.clj
+++ test/binary/test/core.clj
@@ -2,14 +2,83 @@
   (:use [binary.core])
   (:use [clojure.test]))
 
-(deftest byte-array-generation
+;;
+;; testing making byte array
+;;
+
+(deftest to-byte-array-test
   (let [ba (to-byte-array [(byte 1) "test" \a \b \c nil 0x7FFFFFFF])]
-    (is (= binary.core/byte-array-class (class ba)))
+    (is (= byte-array-class (class ba)))
     (is (= (vec ba) [1 116 101 115 116 97 98 99 0 127 -1 -1 -1]))))
 
-(deftest checking-bits
+;;
+;; testing bit-set?
+;;
+
+(deftest bit-set?-test
   (let [n 0x800000000000000000000000000000008000000000000001]
     (is (true? (bit-set? 191 n)))
     (is (true? (bit-set? 63 n)))
     (is (false? (bit-set? 1 n)))
     (is (true? (bit-set? 0 n)))))
+
+;;
+;; test encoding and decoding
+;;
+
+(def id-to-kw
+  {
+   0x00000000 :generic_nack
+   0x0000000b :outbind
+   })
+
+(def kw-to-id
+  {
+   :generic_nack 0x00000000
+   :outbind      0x0000000b})
+
+(def expected (to-byte-array [16 11 2 1 0.9312 10.88745 (float 1.1) [10 20] [4 "text"] ["test string" nil]]))
+
+(def definition [{:name :command-length    :type :int}
+                 {:name :command-id        :type :int :out #(kw-to-id (:command-id %)) :in #(id-to-kw (:command-id %))}
+                 {:name :command-status    :type :int :out #(count (:points %))}
+                 {:name :sequence-number   :type :int}
+                 {:name :fraction          :type :double}
+                 {:name :percentage        :type :double}
+                 {:name :average           :type :float}
+                 {:name :points            :type :int  :number :command-status}
+                 {:name :text              :type :vstring :size 44}
+                 {:name :body              :type :cstring :size 12}])
+
+(deftest encoding-test
+  (let [data expected
+        buff (.flip (.put (java.nio.ByteBuffer/allocate 1000) data))
+        results (decode buff definition)]
+    (is (= 16             (:command-length results)))
+    (is (= :outbind       (:command-id results)))
+    (is (= 2              (:command-status results)))
+    (is (= 1              (:sequence-number results)))
+    (is (= 0.9312         (:fraction results)))
+    (is (= 10.88745       (:percentage results)))
+    (is (= (float 1.1)    (:average results)))
+    (is (= [10 20]        (:points results)))
+    (is (= "text"         (:text results)))
+    (is (= "test string"  (:body results)))))
+
+(deftest decoding-test
+  (let [data  { :command-length 16
+               :command-id :outbind
+               :command-status nil
+               :sequence-number 1
+               :points  [10 20]
+               :fraction 0.9312
+               :percentage 10.88745
+               :average (float 1.1)
+               :text "text"
+               :body "test string longer than it needs to be"}
+        
+        buff (java.nio.ByteBuffer/allocate 1000)
+        results (encode buff definition data)
+        actual  (byte-array (.limit (.flip buff)))
+        _       (.get buff actual (.position results) (.limit results))]
+    (is (= (vec expected) (vec actual)))))
